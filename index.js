7658aaf927fcaf64fd6b947fec6e9e39721926b8
Kenneth Ramos <rainsecrett@gmail.com>
2022-09-01 12:09:40 +0800

    fix

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ let dp = document.querySelector('#dp')
 let user = document.querySelector('#name')
 let count = document.querySelector('#count')
 //change
-dp.src = 'http://scontent-lcy1-1.cdninstagram.com/v/t51.2885-19/274902044_370340178261218_8478867056818183079_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent-lcy1-1.cdninstagram.com&_nc_cat=102&_nc_ohc=NmOxW6mHX9gAX_mJ43D&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AT9vwZORRQjw2Bh4XiSMMJE66-c-5naRNis3Ii3shbJ6cg&oe=63170947&_nc_sid=8fd12b'
+//enable cors
+let cors = 'http://cors.io/?'
+dp.src = cors + 'https://scontent-lcy1-1.cdninstagram.com/v/t51.2885-19/274902044_370340178261218_8478867056818183079_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent-lcy1-1.cdninstagram.com&_nc_cat=102&_nc_ohc=NmOxW6mHX9gAX_mJ43D&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AT9vwZORRQjw2Bh4XiSMMJE66-c-5naRNis3Ii3shbJ6cg&oe=63170947&_nc_sid=8fd12b'
 user.innerHTML = 'Taobao'
 count.innerHTML = '99999999999999'
